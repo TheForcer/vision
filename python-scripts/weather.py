186c3b388ce694ab01789c1651ac83fac3a65756
Felix Alexa <felix@secureim.de>
2019-05-16 11:12:17 +0200

    Reformat code a little and add some comments

--- python-scripts/weather.py
+++ python-scripts/weather.py
@@ -2,19 +2,20 @@ import requests
 import configparser
 import json
 import sys
-from datetime import datetime, timedelta
-from pytz import timezone
 import pytz
+from pytz import timezone
+from datetime import datetime, timedelta
 
-# Import config file and create config sections
+# Import config file and create config section
 config = configparser.ConfigParser()
 config.read('/opt/vision.cfg')
 cfg_weather = config['weather']
 
+# Default timezone and date format
 utc = pytz.utc
 fmt = '%d.%m %H:%M'
 
-# Return a specific stat from the PiHole API
+# Return the json response data for a specific forecast location
 def getForecast(location):
     if location == "default":
         url = "https://api.openweathermap.org/data/2.5/forecast?q="+cfg_weather['location']+"&mode=json&units=metric&lang=de&APPID="+cfg_weather['apikey']
@@ -24,6 +25,7 @@ def getForecast(location):
     json_data = json.loads(requests.request("GET", url, data="", headers="").text)
     return json_data
 
+# Create the string to return if request was either 200 or 400
 def createString(json_data, location):
     if json_data["cod"] == "404":
         return "Die gesuchte Stadt wurde nicht gefunden."
