e6fd627fc62d9b6d0c78afd4a91f68870f412cfd
Felix <git@theforcer.de>
2019-05-17 23:30:53 +0200

    Reformat python code with black

--- python-scripts/pihole.py
+++ python-scripts/pihole.py
@@ -4,13 +4,19 @@ import json
 
 # Import config file and create config sections
 config = configparser.ConfigParser()
-config.read('/opt/vision.cfg')
-cfg_pihole = config['pihole']
+config.read("/opt/vision.cfg")
+cfg_pihole = config["pihole"]
 
 # Return a specific stat from the PiHole API
 def getPiholeStat(stat):
-    url = cfg_pihole['url'] + "/admin/api.php"
+    url = cfg_pihole["url"] + "/admin/api.php"
     json_data = json.loads(requests.request("GET", url, data="", headers="").text)
     return json_data[stat]
 
-print("Von den heutigen "+str(getPiholeStat("dns_queries_today"))+" DNS Anfragen wurden "+str(getPiholeStat("ads_blocked_today"))+" geblockt!")
+print(
+    "Von den heutigen "
+    + str(getPiholeStat("dns_queries_today"))
+    + " DNS Anfragen wurden "
+    + str(getPiholeStat("ads_blocked_today"))
+    + " geblockt!"
+)
--- python-scripts/weather.py
+++ python-scripts/weather.py
@@ -8,19 +8,29 @@ from datetime import datetime, timedelta
 
 # Import config file and create config section
 config = configparser.ConfigParser()
-config.read('/opt/vision.cfg')
-cfg_weather = config['weather']
+config.read("/opt/vision.cfg")
+cfg_weather = config["weather"]
 
 # Default timezone and date format
 utc = pytz.utc
-fmt = '%d.%m %H:%M'
+fmt = "%d.%m %H:%M"
 
 # Return the json response data for a specific forecast location
 def getForecast(location):
     if location == "default":
-        url = "https://api.openweathermap.org/data/2.5/forecast?q="+cfg_weather['location']+"&mode=json&units=metric&lang=de&APPID="+cfg_weather['apikey']
+        url = (
+            "https://api.openweathermap.org/data/2.5/forecast?q="
+            + cfg_weather["location"]
+            + "&mode=json&units=metric&lang=de&APPID="
+            + cfg_weather["apikey"]
+        )
     else:
-        url = "https://api.openweathermap.org/data/2.5/forecast?q="+location+"&mode=json&units=metric&lang=de&APPID="+cfg_weather['apikey']
+        url = (
+            "https://api.openweathermap.org/data/2.5/forecast?q="
+            + location
+            + "&mode=json&units=metric&lang=de&APPID="
+            + cfg_weather["apikey"]
+        )
 
     json_data = json.loads(requests.request("GET", url, data="", headers="").text)
     return json_data
@@ -31,16 +41,24 @@ def createString(json_data, location):
         return "Die gesuchte Stadt wurde nicht gefunden."
     forecast_list = json_data["list"]
     if location == "default":
-        string = "Wetter in: "+cfg_weather['location']+"""\n"""
+        string = "Wetter in: " + cfg_weather["location"] + """\n"""
     else:
-        string = "Wetter in: "+location+"""\n"""
+        string = "Wetter in: " + location + """\n"""
     i = 0
     while i < 3:
         utc_dt = utc.localize(datetime.utcfromtimestamp(forecast_list[i]["dt"]))
-        de_tz = timezone('Europe/Berlin')
+        de_tz = timezone("Europe/Berlin")
         de_dt = utc_dt.astimezone(de_tz)
-        string = string+str(de_dt.strftime(fmt))+" / "+str(forecast_list[i]["main"]["temp"])+"°C / "+forecast_list[i]["weather"][0]["description"]+"""\n"""
-        i+=1
+        string = (
+            string
+            + str(de_dt.strftime(fmt))
+            + " / "
+            + str(forecast_list[i]["main"]["temp"])
+            + "°C / "
+            + forecast_list[i]["weather"][0]["description"]
+            + """\n"""
+        )
+        i += 1
     return string
 
 print(createString(getForecast(sys.argv[1]), sys.argv[1]))
